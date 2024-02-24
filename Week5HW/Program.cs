63a264334ae2e6bb540922973a0441ecbfaa8e91
Derek <Drj2778@email.vccs.edu>
2024-02-24 13:50:26 -0500

    Add project files.

---
+++ Week5HW/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Week5HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b; 
+            a = 20;
+            b = 30;
+
+            while(a <= b) 
+            {
+                Console.WriteLine(a);
+                a++;
+
+                
+            
+            
+            
+            
+            }
+                Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
